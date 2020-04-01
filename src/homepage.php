f2cbc738b50bc1033e1b0ae0fc0b375ea61c234b
Kyriau <jeff.aj.thomson@gmail.com>
2020-03-31 21:54:36 -0700

    added todos to homepage

--- src/homepage.php
+++ src/homepage.php
@@ -1,8 +1,15 @@
+<?php
+	// SQL setup.
+	$user = 'root';
+	$pass = '';
+	$dbname = 'forum';
+	$db = new mysqli('localhost', $user, $pass, $dbname);
+?>
+
 <!DOCTYPE html>
 <html lang=en>
 	<head>
 		<title>Kyriau's Forum Website</title>
-		<base href="../">
 		<link rel="stylesheet" type="text/css" href="css/reset.css">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
@@ -11,21 +18,39 @@
 			<div class="left-header">
 				<h1>The Great Forum</h1>
 				<nav>
-					<a class="header-nav" href="html/homepage.html">Home</a>
+					<a class="header-nav" href="homepage.php">Home</a>
 					<a class="header-nav" href="https://www.google.com">Google</a>
 					<a class="header-nav" href="html/404.html">Other</a>
 				</nav>
 			</div>
+			<!-- TODO: dynamic profile header -->
+			<!-- this should come from session info -->
 			<div class="right-header">
 				<a href="html/profile.html"><img class="profile-pic" src="img/Duck.png"></a>
 				<h3><a href="html/profile.html">Kyriau</a></h3>
 			</div>
 		</div>
+		
+		<!-- Some test db querry. Should be removed asap. -->
+		<?php
+			echo "test";
+			$testResult = $db->query("SELECT * FROM Users");
+			while($row = $testResult->fetch_row()) {
+				foreach($row as $value) {
+					echo $value."<br>";
+				}
+			}
+		?>
+		
 		<div class="main">
+		
+			<!-- TODO: dynamically fetch site news -->
 			<div class="sidebar">
 				<h2>Site News</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Quis risus sed vulputate odio ut enim blandit volutpat. Sed sed risus pretium quam. Fermentum dui faucibus in ornare quam viverra orci sagittis eu. Risus sed vulputate odio ut. Purus viverra accumsan in nisl. Vitae congue eu consequat ac felis donec et. Nulla porttitor massa id neque. Dictum non consectetur a erat nam at lectus urna duis. Non tellus orci ac auctor augue mauris augue neque gravida. Elementum pulvinar etiam non quam lacus suspendisse faucibus. Et tortor consequat id porta nibh venenatis cras. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Libero volutpat sed cras ornare arcu.</p>
 			</div>
+			
+			<!-- TODO: dynamically fetch forums and posts -->
 			<div class="content">
 				<h2>Forums</h2>
 				<div class="content-row">
@@ -62,6 +87,7 @@
 					</div>
 				</div>
 			</div>
+			
 		</div>
 		<div class="footer">
 			<h3>&copy; 2020 Jeff Thomson</h3>
