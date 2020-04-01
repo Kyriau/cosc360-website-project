a4660aa5e4c69cb6ed756342d8c3a3075cd30de4
Kyriau <jeff.aj.thomson@gmail.com>
2020-03-31 18:37:27 -0700

    added php files

---
+++ src/homepage.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang=en>
+	<head>
+		<title>Kyriau's Forum Website</title>
+		<base href="../">
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div class="header">
+			<div class="left-header">
+				<h1>The Great Forum</h1>
+				<nav>
+					<a class="header-nav" href="html/homepage.html">Home</a>
+					<a class="header-nav" href="https://www.google.com">Google</a>
+					<a class="header-nav" href="html/404.html">Other</a>
+				</nav>
+			</div>
+			<div class="right-header">
+				<a href="html/profile.html"><img class="profile-pic" src="img/Duck.png"></a>
+				<h3><a href="html/profile.html">Kyriau</a></h3>
+			</div>
+		</div>
+		<div class="main">
+			<div class="sidebar">
+				<h2>Site News</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Quis risus sed vulputate odio ut enim blandit volutpat. Sed sed risus pretium quam. Fermentum dui faucibus in ornare quam viverra orci sagittis eu. Risus sed vulputate odio ut. Purus viverra accumsan in nisl. Vitae congue eu consequat ac felis donec et. Nulla porttitor massa id neque. Dictum non consectetur a erat nam at lectus urna duis. Non tellus orci ac auctor augue mauris augue neque gravida. Elementum pulvinar etiam non quam lacus suspendisse faucibus. Et tortor consequat id porta nibh venenatis cras. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Libero volutpat sed cras ornare arcu.</p>
+			</div>
+			<div class="content">
+				<h2>Forums</h2>
+				<div class="content-row">
+					<div class="post-title">
+						<h4><a href="html/forum.html">Forum 1</a></h4>
+					</div>
+					<div class="post-preview">
+						<h4>Et sollicitudin ac orci phasellus. Facilisi etiam dignissim diam quis enim lobortis scelerisque. Morbi tempus iaculis urna id. Maecenas pharetra convallis posuere morbi leo urna. Nulla aliquet enim tortor at auctor urna. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Scelerisque fermentum dui faucibus in ornare quam. Viverra accumsan in nisl nisi scelerisque. Eu facilisis sed odio morbi. Tortor dignissim convallis aenean et tortor at risus viverra. Sagittis nisl rhoncus mattis rhoncus urna neque viverra.</h4>
+					</div>
+				</div>
+				<div class="content-row">
+					<div class="post-title">
+						<h4><a href="html/forum.html">Forum 2</a></h4>
+					</div>
+					<div class="post-preview">
+						<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Quis risus sed vulputate odio ut enim blandit volutpat. Sed sed risus pretium quam. Fermentum dui faucibus in ornare quam viverra orci sagittis eu. Risus sed vulputate odio ut. Purus viverra accumsan in nisl. Vitae congue eu consequat ac felis donec et. Nulla porttitor massa id neque. Dictum non consectetur a erat nam at lectus urna duis. Non tellus orci ac auctor augue mauris augue neque gravida. Elementum pulvinar etiam non quam lacus suspendisse faucibus. Et tortor consequat id porta nibh venenatis cras. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Libero volutpat sed cras ornare arcu.</h4>
+					</div>
+				</div>
+				<h2>Posts</h2>
+				<div class="content-row">
+					<div class="post-title">
+						<h4><a href="html/post.html">Post 1</a></h4>
+					</div>
+					<div class="post-preview">
+						<h4>Et magnis dis parturient montes nascetur ridiculus mus. Maecenas volutpat blandit aliquam etiam. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Sit amet consectetur adipiscing elit ut. Pellentesque dignissim enim sit amet venenatis urna cursus eget. Lacinia at quis risus sed vulputate. Euismod elementum nisi quis eleifend. Convallis posuere morbi leo urna molestie at elementum. Volutpat lacus laoreet non curabitur gravida arcu ac. Risus pretium quam vulputate dignissim suspendisse in est ante in. Aliquam malesuada bibendum arcu vitae elementum. Et malesuada fames ac turpis egestas sed. Sagittis nisl rhoncus mattis rhoncus urna neque viverra. Dolor sit amet consectetur adipiscing. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue. Fames ac turpis egestas sed tempus. Vulputate mi sit amet mauris commodo quis imperdiet massa.</h4>
+					</div>
+				</div>
+				<div class="content-row">
+					<div class="post-title">
+						<h4><a href="html/post.html">Post 2</a></h4>
+					</div>
+					<div class="post-preview">
+						<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Quis risus sed vulputate odio ut enim blandit volutpat. Sed sed risus pretium quam. Fermentum dui faucibus in ornare quam viverra orci sagittis eu. Risus sed vulputate odio ut. Purus viverra accumsan in nisl. Vitae congue eu consequat ac felis donec et. Nulla porttitor massa id neque. Dictum non consectetur a erat nam at lectus urna duis. Non tellus orci ac auctor augue mauris augue neque gravida. Elementum pulvinar etiam non quam lacus suspendisse faucibus. Et tortor consequat id porta nibh venenatis cras. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Libero volutpat sed cras ornare arcu.</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<h3>&copy; 2020 Jeff Thomson</h3>
+		</div>
+	</body>
+</html>
