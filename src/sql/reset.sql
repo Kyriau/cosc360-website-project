ff915a4a8759c4ce881705f320b56ae1c1d6e90e
Kyriau <jeff.aj.thomson@gmail.com>
2020-03-31 18:27:00 -0700

    minor changes?

--- src/sql/reset.sql
+++ src/sql/reset.sql
@@ -1,9 +1,14 @@
 USE forum;
 
-DROP TABLE Users;
-DROP TABLE Forums;
-DROP TABLE Threads;
+-- Reset Tables
+
+DROP TABLE UserComments;
 DROP TABLE Comments;
+DROP TABLE Threads;
+DROP TABLE Forums;
+DROP TABLE Users;
+
+-- Create Tables
 
 CREATE TABLE Users(
 
@@ -13,33 +18,65 @@ CREATE TABLE Users(
 	Password VARCHAR(64) NOT NULL,
 	Administrator BOOLEAN,
 	
-	PRIMARY KEY (ID)
+	PRIMARY KEY (ID),
+	INDEX (Email)
 	
 );
 
 CREATE TABLE Forums(
 
 	ID INT NOT NULL AUTO_INCREMENT,
-	Name VARCHAR(64) NOT NULL,
 	Parent INT,
+	UpdateTime DATETIME NOT NULL,
+	Name VARCHAR(64) NOT NULL,
 	
 	PRIMARY KEY (ID),
-	FOREIGN KEY (Parent) REFERENCES Forums(ID)
+	FOREIGN KEY (Parent) REFERENCES Forums(ID),
+	INDEX (UpdateTime)
 	
 );
 
 CREATE TABLE Threads(
 
 	ID INT NOT NULL AUTO_INCREMENT,
+	ForumID INT NOT NULL,
+	UpdateTime DATETIME NOT NULL,
+	Title VARCHAR(64) NOT NULL,
 	
-	PRIMARY KEY (ID)
+	PRIMARY KEY (ID),
+	FOREIGN KEY (ForumID) REFERENCES Forums(ID),
+	INDEX (UpdateTime)
 	
 );
 
 CREATE TABLE Comments(
 
 	ID INT NOT NULL AUTO_INCREMENT,
+	PosterID INT NOT NULL,
+	ThreadID INT NOT NULL,
+	UpdateTime DATETIME NOT NULL,
+	Content TEXT NOT NULL,
 	
-	PRIMARY KEY (ID)
+	PRIMARY KEY (ID),
+	FOREIGN KEY (PosterID) REFERENCES Users(ID),
+	FOREIGN KEY (ThreadID) REFERENCES Threads(ID),
+	INDEX (UpdateTime)
 	
 );
+
+CREATE TABLE UserComments(
+
+	UserID INT NOT NULL,
+	CommentID INT NOT NULL,
+	
+	PRIMARY KEY (UserID, CommentID),
+	FOREIGN KEY (UserID) REFERENCES Users(ID),
+	FOREIGN KEY (CommentID) REFERENCES Comments(ID)
+
+);
+
+-- Populate Tables
+
+INSERT INTO Users(Username, Email, Password, Administrator) VALUES
+	('Kyriau', 'jeff.aj.thomson@alumni.ubc.ca', 'password', TRUE)
+;
